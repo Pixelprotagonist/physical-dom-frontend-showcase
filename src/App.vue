0026f379b947472d4459c529237ae63883cf069d
pixelprotagonist <d.reuschel@hotmail.de>
2024-11-28 23:26:44 +0100

    feat: slotted component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,34 @@
 <script setup>
+import Widget from './components/Widget.vue';
 </script>
 
 <template>
-  <div>
-    This is my nice DnD Dashboard hooray!
+  <div class="root">
+    <h1>
+      This is my nice DnD Dashboard hooray!
+    </h1>
+    <Widget>
+      Some fun will be here soon!
+    </Widget>
+    <Widget>
+      Some more fun will be here soon!
+    </Widget>
+    <Widget>
+      Even fun will be here soon!
+    </Widget>
   </div>
 </template>
 
 <style scoped>
+h1{
+  margin-top: 0;
+}
+
+.root{
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  justify-content: space-between;
+}
 </style>
---
+++ src/components/Widget.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+  <div class="widget">
+    <h2>This is my widget headline</h2>
+    <slot>
+        <div>
+            You should really add some content here
+        </div>
+    </slot>
+  </div>
+</template>
+
+<style scoped>
+.widget {
+    margin: 10px 0;
+    border: none;
+    border-radius: 25px;
+    background-color: bisque;
+    width: 100%;
+}
+</style>
