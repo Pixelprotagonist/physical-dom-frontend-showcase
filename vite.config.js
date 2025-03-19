7d4bd3aa7322bd017de9a08070b9340577587d09
Pixelprotagonist <D.reuschel@hotmail.de>
2025-03-19 15:22:33 +0100

    feat: backend server makes requests to printess

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    cors: {
+      // dynamically replace with backend url from .env if this will ever be deployed
+      origin: 'http://localhost:3000',
+    },
+  },
+  build: {
+    // generate .vite/manifest.json in outDir
+    manifest: true,
+    rollupOptions: {
+      // overwrite default .html entry
+      input: '/path/to/main.js',
+    },
+  },
 })
