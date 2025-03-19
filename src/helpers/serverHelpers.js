824789d6460bbc7fe42e4edbc358c41c111da0e4
Pixelprotagonist <D.reuschel@hotmail.de>
2025-03-19 16:46:01 +0100

    fix: move printess functions out of main server file

--- src/helpers/serverHelpers.js
+++ src/helpers/serverHelpers.js
@@ -3,86 +3,87 @@ function sleep(ms) {
   }
 
 async function postJson(url, token, model) {
-    return await fetch(url, {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(model)
-    });
+  return await fetch(url, {
+    method: "POST",
+    mode: "cors",
+    cache: "no-cache",
+    headers: {
+    "Content-Type": "application/json",
+    "Authorization": "Bearer " + token
+    },
+    redirect: "follow",
+    referrerPolicy: "no-referrer",
+    body: JSON.stringify(model)
+  });
 }
 
 async function sendToProduction(token, templateNameOrSaveToken) {
-    const model = {
-        "templateName": templateNameOrSaveToken,
-        "origin": "YOUR PRODUCTION SYSTEM NAME",
-        "outputSettings": {
-        "dpi": 300
-        }
-    };
-    const response = await postJson("https://api.printess.com/production/produce", token, model);
-
-    return response;
+  const model = {
+    "templateName": templateNameOrSaveToken,
+    "origin": "YOUR PRODUCTION SYSTEM NAME",
+    "outputSettings": {
+    "dpi": 300
     }
+  };
+  const response = await postJson("https://api.printess.com/production/produce", token, model);
 
-    async function getStatus(token, jobId) {
-    const model = { "jobId": jobId };
-    const response = await postJson("https://api.printess.com/production/status/get", token, model);
+  return response;
+  }
 
-    if (!response.ok) {
-        throw new Error(`Cannot retrieve status: ${response.statusText} (statusCode=${response.status})`);
-    }
+  async function getStatus(token, jobId) {
+  const model = { "jobId": jobId };
+  const response = await postJson("https://api.printess.com/production/status/get", token, model);
 
-    const apiStatus = await response.json();
+  if (!response.ok) {
+    throw new Error(`Cannot retrieve status: ${response.statusText} (statusCode=${response.status})`);
+  }
 
-    return apiStatus;
+  const apiStatus = await response.json();
+
+  return apiStatus;
 }
 
 export async function producePdf(token, templateNameOrSaveToken) {
-    const produceResponse = await sendToProduction(token, templateNameOrSaveToken);
+  console.log('we arrive');
+  console.log(token, templateNameOrSaveToken)
+  const produceResponse = await sendToProduction(token, templateNameOrSaveToken);
 
-    if (!produceResponse.ok) {
-        const errorMessage = await produceResponse.text();
-        throw new Error("Cannot produce pdf error=" + errorMessage + ", statusCode=" + produceResponse.status);
+  if (!produceResponse.ok) {
+    const errorMessage = await produceResponse.text();
+    throw new Error("Cannot produce pdf error=" + errorMessage + ", statusCode=" + produceResponse.status);
+  }
+
+  const produceJson = await produceResponse.json();
+  const jobId = produceJson.jobId;
+  const statusModel = { "jobId": jobId };
+
+  let isFinished = false;
+  let status;
+
+  do {
+    status = await getStatus(token, jobId);
+    isFinished = status.isFinalStatus;
+
+    if (!isFinished) {
+    await sleep(1000);
+    }
+  } while (!isFinished);
+
+  if (status.isSuccess) {
+    const pdfs = Object.keys(status.result.r).map(key => ({ document: key, url: status.result.r[key] }));
+    console.log("###", pdfs[0].url)
+
+    return pdfs[0].url;
+
+    // this handles picture output (png, jpg, tif) if you set output type
+    if (status.result.p) {
+    for (let i = 0; i < status.result.p.length; i++) {
+        const distributionFile = status.result.p[i];
+        window.open(distributionFile.u, "_blank");
+    }
     }
 
-    const produceJson = await produceResponse.json();
-    const jobId = produceJson.jobId;
-    const statusModel = { "jobId": jobId };
-
-    let isFinished = false;
-    let status;
-
-    do {
-        status = await getStatus(token, jobId);
-        isFinished = status.isFinalStatus;
-
-        if (!isFinished) {
-        await sleep(1000);
-        }
-    } while (!isFinished);
-
-    if (status.isSuccess) {
-        const pdfs = Object.keys(status.result.r).map(key => ({ document: key, url: status.result.r[key] }));
-
-        for (const document of pdfs) {
-        window.open(document.url, "_blank");
-        }
-
-        // this handles picture output (png, jpg, tif) if you set output type
-        if (status.result.p) {
-        for (let i = 0; i < status.result.p.length; i++) {
-            const distributionFile = status.result.p[i];
-            window.open(distributionFile.u, "_blank");
-        }
-        }
-
-    } else {
-        alert("Cannot render pdf: " + status.errorDetails);
-    }
+  } else {
+      alert("Cannot render pdf: " + status.errorDetails);
+  }
 }
--- src/server.js
+++ src/server.js
@@ -3,101 +3,16 @@ import ViteExpress from "vite-express";
 import cors from 'cors';
 import dotenv from 'dotenv';
 
-
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-  }
-
-async function postJson(url, token, model) {
-    return await fetch(url, {
-        method: "POST",
-        mode: "cors",
-        cache: "no-cache",
-        headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + token
-        },
-        redirect: "follow",
-        referrerPolicy: "no-referrer",
-        body: JSON.stringify(model)
-    });
-}
-
-async function sendToProduction(token, templateNameOrSaveToken) {
-    const model = {
-        "templateName": templateNameOrSaveToken,
-        "origin": "YOUR PRODUCTION SYSTEM NAME",
-        "outputSettings": {
-        "dpi": 300
-        }
-    };
-    const response = await postJson("https://api.printess.com/production/produce", token, model);
-
-    return response;
-    }
-
-    async function getStatus(token, jobId) {
-    const model = { "jobId": jobId };
-    const response = await postJson("https://api.printess.com/production/status/get", token, model);
-
-    if (!response.ok) {
-        throw new Error(`Cannot retrieve status: ${response.statusText} (statusCode=${response.status})`);
-    }
-
-    const apiStatus = await response.json();
-
-    return apiStatus;
-}
-
-export async function producePdf(token, templateNameOrSaveToken) {
-    const produceResponse = await sendToProduction(token, templateNameOrSaveToken);
-
-    if (!produceResponse.ok) {
-        const errorMessage = await produceResponse.text();
-        throw new Error("Cannot produce pdf error=" + errorMessage + ", statusCode=" + produceResponse.status);
-    }
-
-    const produceJson = await produceResponse.json();
-    const jobId = produceJson.jobId;
-    const statusModel = { "jobId": jobId };
-
-    let isFinished = false;
-    let status;
-
-    do {
-        status = await getStatus(token, jobId);
-        isFinished = status.isFinalStatus;
-
-        if (!isFinished) {
-            await sleep(1000);
-        }
-    } while (!isFinished);
-
-    if (status.isSuccess) {
-        const pdfs = Object.keys(status.result.r).map(key => ({ document: key, url: status.result.r[key] }));
-
-        return pdfs[0].url;
-
-        // this handles picture output (png, jpg, tif) if you set output type
-        if (status.result.p) {
-            for (let i = 0; i < status.result.p.length; i++) {
-                const distributionFile = status.result.p[i];
-                window.open(distributionFile.u, "_blank");
-            }
-        }
-
-    } else {
-        alert("Cannot render pdf: " + status.errorDetails);
-    }
-}
+import { producePdf } from "./helpers/serverHelpers.js";
 
 const app = express();
 app.use(cors())
-dotenv.config({path:"../.env"});
+dotenv.config({path:".env"});
+
 
 app.get("/api/print", async (req, res) => {
-    const pdfUrl = await producePdf(process.env.API_TOKEN, req.query.token)
-    res.send(pdfUrl);
+  const pdfUrl = await producePdf(process.env.API_TOKEN, req.query.token)
+  res.send(pdfUrl);
 });
 
 ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
